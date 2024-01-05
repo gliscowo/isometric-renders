aa9edc7d50427ac45f7226c09f7e66ae47b54f97
glisco <glisco03@web.de>
2024-01-05 19:27:04 +0100

    1.20.4

--- src/main/java/com/glisco/isometricrenders/widget/PropertyCheckboxComponent.java
+++ src/main/java/com/glisco/isometricrenders/widget/PropertyCheckboxComponent.java
@@ -2,7 +2,6 @@ package com.glisco.isometricrenders.widget;
 
 import com.glisco.isometricrenders.property.Property;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.widget.CheckboxWidget;
 import net.minecraft.text.Text;
 
@@ -11,20 +10,8 @@ public class PropertyCheckboxComponent extends CheckboxWidget {
     private final Property<Boolean> property;
 
     public PropertyCheckboxComponent(Text message, Property<Boolean> property) {
-        super(0, 0, 20, 20, message, property.get(), false);
+        super(0, 0, message, MinecraftClient.getInstance().textRenderer, property.get(), (checkbox, checked) -> {});
         this.property = property;
-    }
-
-    @Override
-    public void renderButton(DrawContext context, int mouseX, int mouseY, float delta) {
-        super.renderButton(context, mouseX, mouseY, delta);
-        context.drawText(
-                MinecraftClient.getInstance().textRenderer,
-                this.getMessage(),
-                this.getX() + 24,
-                (int) (this.getY() + (this.height - 8) / 2f),
-                0xAAAAAA, false
-        );
     }
 
     @Override
